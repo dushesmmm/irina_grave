960598b1dfdf153488f9e079ac3feb03f3600e5a
dushes <dushesmmm@gmail.com>
2024-10-28 12:14:08 +0300

    video added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { FaPlayCircle } from 'react-icons/fa';
 import classes from './about.module.css';
 import magz from '../../../public/images/magazines.png';
+import play from '../../../public/images/about us/play.svg'
 
 const About = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -22,20 +23,19 @@ const About = () => {
       <div className={classes.videoWrapper}>
         {!isPlaying && (
           <div className={classes.overlay} onClick={handlePlay}>
-            <FaPlayCircle className={classes.playIcon} />
+            <Image alt='старт видео' src={play} className={classes.playIcon} />
           </div>
         )}
         <video
           id="about-video"
           className={classes.video}
-          preload="none"
+          preload="true"
           controls={isPlaying}
           poster="/images/about us/prev.jpg"
         >
           <source src="/images/about us/video-irina-grave.mp4" type="video/mp4" />
         </video>
       </div>
-
       <div className={classes.wrapper}>
         <Image src={magz} alt="Magazines" />
       </div>
