9eaf9660f20be7fef943e3617171ac65a9de7044
dushes <dushesmmm@gmail.com>
2024-10-29 13:25:47 +0300

    header optimization

--- src/app/UI/header/Header.jsx
+++ src/app/UI/header/Header.jsx
@@ -1,9 +1,32 @@
+'use client'
+
+import { useState, useEffect } from "react";
 import classes from "./Header.module.css";
 import logo from "../../../../public/images/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
+import './Header.css'
 
 const Header = ({ dark }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (menuOpen) {
+      const scrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+    } else {
+      const scrollY = document.body.style.top;
+      document.body.style.position = '';
+      document.body.style.top = '';
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    }
+  }, [menuOpen]);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={`${classes.wrapper} ${dark ? classes.dark : ""}`}>
       <Link href='/' className={classes.imageWrapper}>
@@ -15,7 +38,8 @@ const Header = ({ dark }) => {
           className={`${dark ? classes.invert : ""}`}
         />
       </Link>
-      <div className={classes.nav}>
+      <div className={`${menuOpen ? 'burger open' : 'burger'} ${dark ? classes.invert : ''}`} onClick={toggleMenu}><span></span></div>
+      <div className={`${classes.nav} ${menuOpen ? classes.active : ""} ${dark ? classes.grey : ''}`}>
         <Link
           href='/gallery'
           className={`${classes.navigation} ${dark ? classes.invert : ""}`}
