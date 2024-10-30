272c779b64e2e587033a0d9df847de51ed0a2046
dushes <dushesmmm@gmail.com>
2024-10-30 23:08:11 +0300

    one more

--- src/app/UI/brands/Brands.jsx
+++ src/app/UI/brands/Brands.jsx
@@ -9,15 +9,16 @@ import image4 from '../../../../public/images/brands/lofficiel.svg';
 import image5 from '../../../../public/images/brands/design mate.svg';
 import image6 from '../../../../public/images/brands/kommersant.svg';
 import image7 from '../../../../public/images/brands/salon.svg';
-import image8 from '../../../../public/images/brands/mydecor.svg';
+import image8 from '../../../../public/images/brands/beta.svg';
+import image9 from '../../../../public/images/brands/mydecor.svg';
 
 const Brands = () => {
-    const brands = [image1, image2, image3, image4, image5, image6, image7, image8];
+    const brands = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
 
   return (
     <div className={classes.brands}>
         {brands.map((image, index) => (
-          <Image key={index} src={image} alt={`Бренд ${index + 1}`} style={{ width: '23%', marginBottom: 40, maxHeight: 200 }} />
+          <Image key={index} quality={100} src={image} alt={`Бренд ${index + 1}`} style={{ marginBottom: 40, maxHeight: 200 }} />
         ))}
       </div>
   )
